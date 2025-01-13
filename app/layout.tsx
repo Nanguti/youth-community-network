99ccf6c39cb54bd9879705ca283b3b67bcd58b9f
Wanyonyi <g.nanguti@gmail.com>
2025-01-13 12:18:39 +0300

    UI Improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${fontSans.variable} font-sans pt-[64px]`}>
+      <body className={`${fontSans.variable} font-sans pt-[83px]`}>
         <Navbar />
         {children}
         <Footer />
